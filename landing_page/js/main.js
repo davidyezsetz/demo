478b62c28e6ae91181fc473f2d46f38a302059ea
David Setz <david.setz@kaufhof.de>
2017-05-01 19:23:47 +0200

    Add accordion functionality

--- landing_page/js/main.js
+++ landing_page/js/main.js
@@ -1,1 +1,8 @@
-console.log('loaded');
+const accordionActiveClass = 'accordion--active';
+const accordionTrigger = document.querySelectorAll('.accordion');
+const accordionHandler = (event) =>
+  event.target.classList.toggle(accordionActiveClass);
+
+for (const item of accordionTrigger) {
+  item.addEventListener('click', accordionHandler);
+}
